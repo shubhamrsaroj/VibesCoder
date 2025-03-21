5db37b8da6952b06090aaf81b8db37a3fe838969
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2025-03-22 03:36:13 +0530

    newwww

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
       'eslint:recommended',
       'plugin:react/recommended',
       'plugin:react-hooks/recommended',
+      'plugin:jsx-a11y/recommended'
     ],
     parserOptions: {
       ecmaFeatures: {
@@ -16,16 +17,17 @@ module.exports = {
       ecmaVersion: 12,
       sourceType: 'module',
     },
-    plugins: ['react', 'react-hooks'],
+    plugins: ['react', 'react-hooks', 'jsx-a11y'],
     rules: {
       'no-unused-vars': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
-      'jsx-a11y/anchor-is-valid': 'warn',
+      'jsx-a11y/anchor-is-valid': 'off',
       'default-case': 'warn',
       'no-use-before-define': 'warn',
-      'array-callback-return': 'warn'
+      'array-callback-return': 'warn',
+      'react/no-unescaped-entities': 'off'
     },
     settings: {
       react: {
