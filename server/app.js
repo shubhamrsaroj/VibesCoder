8c9cd483b862840203fddf45c1011efb5a1e438d
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2025-03-22 06:53:15 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,17 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Enable pre-flight requests
-app.options('*', cors());
-
 app.use(cors({
-  origin: ['https://67de049ac274ec00082a92a5--peppy-snickerdoodle-ccc36f.netlify.app', 'http://localhost:3000'],
+  origin: true, // Allow all origins temporarily for testing
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
-  exposedHeaders: ['Content-Range', 'X-Content-Range']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With', 'Access-Control-Allow-Origin'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
-// ...existing code...
+// Add headers middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 module.exports = app;
+
